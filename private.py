a9cd31fbff1fcab61dc1f6f10b272d64c3a31447
Kera <95004283+itzkera@users.noreply.github.com>
2026-03-04 17:59:56 -0500

    Update private.py

--- private.py
+++ private.py
@@ -1,7 +1,22 @@
 from mitmproxy import http
 
+EPIC_GAMES = (
+    "game-social.epicgames.com",
+    "ol.epicgames.com",
+    "ol.epicgames.net",
+    "on.epicgames.com",
+    "ak.epicgames.com",
+    "epicgames.dev",
+)
+
 def request(flow: http.HTTPFlow) -> None:
-    if ".ol.epicgames.com" in flow.request.pretty_host:
+    host = flow.request.pretty_host
+
+    if host and host.endswith(EPIC_GAMES):
+        print(f"Redirecting {host} to localhost lol")
+
+        # send it to my backend instead
         flow.request.scheme = "http"
-        flow.request.host = ""
+        flow.request.host = "127.0.0.1"
         flow.request.port = 3551
+        flow.request.headers["Host"] = host
